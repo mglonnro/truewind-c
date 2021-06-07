a76e4675f237614964d169ae4901479996873fd3
Mikael Lönnroth <mikael@humeko.com>
2021-06-07 21:37:47 +0300

    Add truewind.h

---
+++ truewind.h
@@ -0,0 +1,30 @@
+struct tw_input {
+ double bspd;
+        double sog;
+        double cog;
+        double aws;
+        double awa;
+        double heading;
+        double variation;
+        double roll;
+        double pitch;
+        double K;
+        char *speedunit;
+};
+
+struct tw_output {
+  double awa;
+  double aws;
+  double leeway;  
+  double stw;
+  double vmg;
+  double tws;
+  double twa;
+  double twd;
+  double soc;
+  double doc;
+};
+
+struct tw_input getAttitudeCorrections(struct tw_input src) ;
+struct tw_output get_true(
+        struct tw_input s);
